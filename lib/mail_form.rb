6aaf49f4d9b1092c00a88c8c7d1ff125c7058b26
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2012-06-25 18:03:31 -0300

    Remove old Callbacks autoload

--- lib/mail_form.rb
+++ lib/mail_form.rb
@@ -1,6 +1,5 @@
 module MailForm
   autoload :Base,      'mail_form/base'
-  autoload :Callbacks, 'mail_form/callbacks'
   autoload :Delivery,  'mail_form/delivery'
   autoload :Notifier,  'mail_form/notifier'
   autoload :Shim,      'mail_form/shim'
